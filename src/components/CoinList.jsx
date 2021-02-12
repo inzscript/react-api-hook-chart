75474db1d9ede1940c136370d0bb8271e917279d
inzscript <inzscript@gmail.com>
2021-02-11 22:28:09 -0800

    Set Loading State

--- src/components/CoinList.jsx
+++ src/components/CoinList.jsx
@@ -6,23 +6,28 @@ import { WatchListContext } from '../contexts/watchListContext';
 function CoinList() {
     const [coins, setCoins] = useState([]);
     // Destructure the watchlist props
-    const { watchList } = useContext( WatchListContext )
+    const { watchList } = useContext( WatchListContext );
+    // Loading screen State
+    const [ isLoading, setIsLoading ] = useState(false);
     // console.log( watchList );
     useEffect (() => {
         const fetchData = async () => {
+            setIsLoading(true);
             // GET call to coinGecko
             const response = await coinGecko.get("/coins/markets", {
                 params: {
                     vs_currency: "usd",
                     // Use context to generate list of bitcoins
                     ids: watchList.join(","),
-                }
-            })
+                },
+            });
+            setCoins(response.data);
+            setIsLoading(false);
             console.log(response.data);
         }
 
         fetchData()
-    })
+    }, [])
     return (
         <div>
             
