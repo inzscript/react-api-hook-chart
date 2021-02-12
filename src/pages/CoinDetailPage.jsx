c8ed9a2d54c574bc9f6beeef93488442a9db2de0
inzscript <inzscript@gmail.com>
2021-02-12 01:26:59 -0800

    Fetching Data for DetailPage

--- src/pages/CoinDetailPage.jsx
+++ src/pages/CoinDetailPage.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import HistoryChart from '../components/HistoryChart';
+import CoinData from '../components/CoinData';
+import coinGecko from '../apis/coinGecko';
 
 function CoinDetailPage() {
     const {id} = useParams();
     const [coinData, setCoinData] = useState({});
     const [isLoading, setIsLoading] = useState(false);
-
-    console.log( id );
 
     // const formatData = (data) => {
     // return data.map((el) => {
@@ -16,6 +17,58 @@ function CoinDetailPage() {
     //     };
     // });
     // };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setIsLoading(true);
+            // Destructure the request information needed.
+            const [day, week, year, detail] = await Promise.all([
+                coinGecko.get(`/coins/${id}/market_chart/`, {
+                    params: {
+                        vs_currency: "usd",
+                        days: "1",
+                    },
+                }),
+                coinGecko.get(`/coins/${id}/market_chart/`, {
+                    params: {
+                        vs_currency: "usd",
+                        days: "7",
+                    },
+                }),
+                coinGecko.get(`/coins/${id}/market_chart/`, {
+                    params: {
+                        vs_currency: "usd",
+                        days: "365",
+                    },
+                }),
+                coinGecko.get("/coins/markets/", {
+                    params: {
+                        vs_currency: "usd",
+                        ids: id,
+                    },
+                }),
+            ]);
+            console.log(day);
+        
+            setCoinData({
+                day: day.data.prices,
+                week: week.data.prices,
+                year: year.data.prices,
+                // Get the first element of the json data
+                detail: detail.data[0],
+            });
+            setIsLoading(false);
+        };
+    
+        fetchData();
+    }, []);
+
+    const renderData = () => {
+        <div className="coinlist">
+            <HistoryChart />
+            <CoinData />
+        </div>
+    };
 
     return (
         <div>
