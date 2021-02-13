78a624f3dc53ff058ad3af5996439a6a42904570
inzscript <inzscript@gmail.com>
2021-02-12 16:12:44 -0800

    Button to Add Coin

--- src/components/AddCoin.jsx
+++ src/components/AddCoin.jsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React, { useState, useContext } from 'react';
+import { WatchListContext } from '../contexts/watchListContext';
 
+// Uses bootsrap dropdown
 function AddCoin() {
+    const [isActive, setIsActive] = useState(false);
+    const { addCoin } = useContext(WatchListContext);
+    const availableCoins = [
+        "bitcoin",
+        "ethereum",
+        "ripple",
+        "tether",
+        "bitcoin-cash",
+        "litecoin",
+        "eos",
+        "okb",
+        "tezos",
+        "cardano",
+    ];
+
+    // Add coin and close dropdown menu.
+    const handleClick = (coin) => {
+        addCoin(coin);
+        setIsActive(false);
+    };
+
     return (
-        <div>
-            
+        <div className="dropdown">
+        <button
+            // toggle button visible
+            onClick={() => setIsActive(!isActive)}
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+        >
+            Add Coin
+        </button>
+        {/* dynamically render to show dropdown list */}
+        <div className={isActive ? "dropdown-menu show" : "dropdown-menu"}>
+            {/* Reiterate over list of coins */}
+            {availableCoins.map((el) => {
+            return (
+                <a
+                    onClick={() => handleClick(el)}
+                    href="#"
+                    className="dropdown-item"
+                    >
+                    {el}
+                </a>
+            );
+            })}
+        </div>
         </div>
     )
 }
