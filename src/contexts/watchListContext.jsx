60af35ca6cf73e49a2ce5cae5d6075d84f45f0a5
inzscript <inzscript@gmail.com>
2021-02-12 16:23:38 -0800

    Local Storage

--- src/contexts/watchListContext.jsx
+++ src/contexts/watchListContext.jsx
@@ -3,10 +3,10 @@ import React, { createContext, useState, useEffect } from "react";
 export const WatchListContext = createContext();
 
 export const WatchListContextProvider = (props) => {
-  console.log();
+  console.log(localStorage.getItem("watchList").split(","));
   // Create a base list of Crypto Currency for Dropdown
   const [watchList, setWatchList] = useState(
-    // localStorage.getItem("watchList").split(",") || 
+    localStorage.getItem("watchList").split(",") || 
     [
       "bitcoin",
       "ethereum",
@@ -15,8 +15,10 @@ export const WatchListContextProvider = (props) => {
     ]
   );
 
+  // Add list of coins to local storage.
   useEffect(() => {
     localStorage.setItem("watchList", watchList);
+    // add dependency to watchList
   }, [watchList]);
 
   // Remove coin from watchlist onClick event.
@@ -28,7 +30,9 @@ export const WatchListContextProvider = (props) => {
     );
   };
 
+  // Add coin to list
   const addCoin = (coin) => {
+    // Check to see if the coin is not already in the list. if value > 0 the it's in the list.
     if (watchList.indexOf(coin) === -1) {
       setWatchList([...watchList, coin]);
     }
